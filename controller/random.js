64445e001f2dc10833f2dd495407e78c88cd2c68
Sodynoizz_TH <tepza5556789@gmail.com>
2023-10-15 17:01:19 +0700

    update: minor update

--- controller/random.js
+++ controller/random.js
@@ -2,37 +2,46 @@ import fs from "fs";
 import path from "path";
 import FoodModel from "../models/foodModel.js";
 import { getRandomNumber } from "../utils/utils.js";
-export const RandomFoods = async (req, res) => {
+
+const getRandomFood = async (filter) => {
+  try {
+    const modelCounts = await FoodModel.countDocuments(filter);
+    const randomIndex = getRandomNumber(1, modelCounts + 1);
+    return await FoodModel.findOne(filter).skip(randomIndex - 1);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+    throw new Error("Internal Server Error");
+  }
+};
+
+export const RandomFoodsByTypes = async (req, res) => {
   const { foodtype } = req.body;
+  try {
+    const filter = foodtype === "all" ? {} : { foodtype };
+    const randomFood = await getRandomFood(filter);
 
-  if (foodtype === "all" || foodtype === "") {
-    try {
-      const modelCounts = await FoodModel.countDocuments({});
-      const randomIndex = Math.floor(getRandomNumber(1, modelCounts + 1));
-      const foods = await FoodModel.findOne({}).skip(randomIndex - 1);
-      return res.status(200).send(foods);
-    } catch (err) {
-      console.error(`Error: ${err}`);
-      return res.status(500).send({ error: "Internal Server Error" });
+    if (!randomFood) {
+      return res.status(400).send({ err: `Cannot find foods in ${foodtype} category` });
     }
-  } else {
-    try {
-      const randomFoodsByType = await FoodModel.find({ foodtype: foodtype });
-      const randomIndex = Math.floor(
-        getRandomNumber(0, randomFoodsByType.length - 1)
-      );
 
-      if (randomIndex === -1) {
-        return res
-          .status(400)
-          .send({ err: `Cannot found foods in ${foodtype} category` });
-      }
-      const foods = randomFoodsByType[randomIndex];
-      return res.status(200).send(foods);
-    } catch (err) {
-      console.error(`Error: ${err}`);
-      return res.status(500).send({ err: "Internal Server Error" });
+    return res.status(200).send(randomFood);
+  } catch (err) {
+    return res.status(500).send({ error: err.message });
+  }
+};
+
+export const RandomFoodsByPlaces = async (req, res) => {
+  const { place } = req.body;
+  try {
+    const filter = place === "all" ? {} : { place };
+    const randomFood = await getRandomFood(filter);
+    if (!randomFood) {
+      return res.status(400).send({ err: `Cannot find foods in ${place} category` });
     }
+
+    return res.status(200).send(randomFood);
+  } catch (err) {
+    return res.status(500).send({ error: err.message });
   }
 };
 
@@ -83,6 +92,21 @@ export const SearchMenuByID = async (req, res) => {
       return res.status(200).send(foods);
     } else {
       return res.status(404).send({ err: "Cannot find a matching food id" });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send({ err: "Internal Server Error" });
+  }
+};
+
+export const SearchMenuByPlace = async (req, res) => {
+  const { place } = req.body;
+  try {
+    const foods = await FoodModel.findOne({ place: place });
+    if (foods) {
+      return res.status(200).send(foods);
+    } else {
+      return res.status(404).send({ err: "Cannot find a matching place" });
     }
   } catch (err) {
     console.error(err);
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import {
   AddFoodToDB,
-  RandomFoods,
+  RandomFoodsByTypes,
+  RandomFoodsByPlaces,
   SearchMenuByName,
   SearchMenuByID,
+  SearchMenuByPlace
 } from "../controller/random.js";
 
 const router = Router();
@@ -13,19 +15,29 @@ const router = Router();
 // @access Private
 router.post("/add-foods", AddFoodToDB);
 
-// @desc Add foods to database
-// @route POST /random-foods
+// @desc Add foods to database by type
+// @route POST /random-foods-by-type
 // @access Public
-router.post("/random-foods", RandomFoods);
+router.post("/random-foods-by-types", RandomFoodsByTypes);
+
+// @desc Add foods to database by type
+// @route POST /random-foods-by-type
+// @access Public
+router.post("/random-foods-by-places", RandomFoodsByPlaces);
 
 // @desc Search for menu by name
 // @route POST /search-menu-by-name
 // @access Public
 router.post("/search-menu-by-name", SearchMenuByName);
 
-// @desc Search for menu by name
-// @route POST /search-menu
+// @desc Search for menu by id
+// @route POST /search-menu-by-id
 // @access Public
 router.get("/search-menu-by-id/:id", SearchMenuByID);
+
+// @desc Search for menu by place
+// @route POST /search-menu-by-place
+// @access Public
+router.post("/search-menu-by-place", SearchMenuByPlace);
 
 export default router;
